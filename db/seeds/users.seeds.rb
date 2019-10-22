5c05110ba917db15b655e772b04f295f89e53090
Thales Augusto <thallles.ramos@gmail.com>
2019-10-22 09:21:42 -0300

    Consertando os seeds por causa nova modelagem do banco

--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -1,19 +1,16 @@
-after :courses, :addresses do
+after :courses do
   User.find_or_create_by(name: 'Thales Augusto', email: 'thales@gmail.com',
-                         semester: 8, course: Course.first,
-                         address: Address.last) do |user|
+                         semester: 8, course: Course.first) do |user|
     user.password = '123456'
   end
 
   User.find_or_create_by(name: 'Teste', email: 'teste@gmail.com',
-                        semester: 5, course: Course.third,
-                        address: Address.second) do |user|
+                         semester: 5, course: Course.third) do |user|
     user.password = '123456'
   end
 
   User.find_or_create_by(name: 'Arthur Ramos', email: 'arthur@gmail.com',
-                        semester: 1, course: Course.second,
-                        address: Address.third) do |user|
+                         semester: 1, course: Course.second) do |user|
     user.password = '123456'
   end
 end
