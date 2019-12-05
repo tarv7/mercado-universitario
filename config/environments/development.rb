01a69f97a9a4046e7d930530d651d9056ebd4c88
Thales Augusto <thallles.ramos@gmail.com>
2019-12-05 00:42:54 -0300

    ActionMailer configurado e funcionando

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,18 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      'mercado.universitario.uesc@gmail.com',
+    password:       Rails.application.credentials.mail_password,
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
