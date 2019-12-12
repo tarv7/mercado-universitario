6423fcc4795cea2e9b2ca29e34e8fd8e65e3af0e
Thales Augusto <thallles.ramos@gmail.com>
2019-12-12 20:07:06 -0300

    Colocando imagens estaticas em pasta diferente para nao perder a path

--- db/seeds/production/categories.seeds.rb
+++ db/seeds/production/categories.seeds.rb
@@ -1,27 +1,27 @@
 Category.find_or_create_by(name: 'Comidas') do |c|
-  c.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'categories', 'comidas.jpg')), filename: 'comidas.jpg')
+  c.image.attach(io: File.open(Rails.root.join('public', 'images', 'categories', 'comidas.jpg')), filename: 'comidas.jpg')
 end
 
 Category.find_or_create_by(name: 'Vestimentas') do |c|
-  c.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'categories', 'vestimentas.jpg')), filename: 'vestimentas.jpg')
+  c.image.attach(io: File.open(Rails.root.join('public', 'images', 'categories', 'vestimentas.jpg')), filename: 'vestimentas.jpg')
 end
 
 Category.find_or_create_by(name: 'Livros') do |c|
-  c.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'categories', 'livros.jpeg')), filename: 'livros.jpeg')
+  c.image.attach(io: File.open(Rails.root.join('public', 'images', 'categories', 'livros.jpeg')), filename: 'livros.jpeg')
 end
 
 Category.find_or_create_by(name: 'Moradias') do |c|
-  c.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'categories', 'moradias.jpeg')), filename: 'moradias.jpeg')
+  c.image.attach(io: File.open(Rails.root.join('public', 'images', 'categories', 'moradias.jpeg')), filename: 'moradias.jpeg')
 end
 
 Category.find_or_create_by(name: 'Serviços') do |c|
-  c.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'categories', 'servicos.jpg')), filename: 'servicos.jpg')
+  c.image.attach(io: File.open(Rails.root.join('public', 'images', 'categories', 'servicos.jpg')), filename: 'servicos.jpg')
 end
 
 Category.find_or_create_by(name: 'Artes') do |c|
-  c.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'categories', 'artes.jpg')), filename: 'artes.jpg')
+  c.image.attach(io: File.open(Rails.root.join('public', 'images', 'categories', 'artes.jpg')), filename: 'artes.jpg')
 end
 
 Category.find_or_create_by(name: 'Outros') do |c|
-  c.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'categories', 'outros.png')), filename: 'outros.png')
+  c.image.attach(io: File.open(Rails.root.join('public', 'images', 'categories', 'outros.png')), filename: 'outros.png')
 end
